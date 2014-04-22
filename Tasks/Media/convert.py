353cb9260a88e38531738b9d17a17cb8aa650a3f
harlo <harlo.holmes@gmail.com>
2014-04-22 10:57:26 -0400

    log stubs

--- Tasks/Media/convert.py
+++ Tasks/Media/convert.py
@@ -3,6 +3,34 @@ from __future__ import absolute_import
 from vars import CELERY_STUB as celery_app
 
 @celery_app.task
+def unzipAndEvaluateArchive(task):
+	task_tag = "UNZIPPING FILE"
+	print "\n\n************** %s [START] ******************\n" % task_tag
+	print "image preprocessing at %s" % task.doc_id
+	task.setStatus(412)
+		
+	from lib.Worker.Models.uv_document import UnveillanceDocument
+	
+	from conf import DEBUG
+	from vars import ASSET_TAGS
+	
+	media = UnveillanceDocument(_id=task.doc_id)
+	if media is None:
+		print "DOC IS NONE"
+		print "\n\n************** %s [ERROR] ******************\n" % task_tag
+		return
+	
+	zip = media.getAsset(task.file_name, return_only="path")
+	print zip
+	
+	'''
+		could be either a source or a j3mlog
+	'''
+	
+	task.finish()
+	print "\n\n************** %s [END] ******************\n" % task_tag
+
+@celery_app.task
 def audioConvert(task):
 	task_tag = "CONVERTING SOME AUDIO"
 	print "\n\n************** %s [START] ******************\n" % task_tag
