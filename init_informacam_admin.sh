3d13d907099e1ca45b4570d7b3da1fb6b6f2aa2c
harlo <harlo@guardianproject.info>
2014-06-02 21:21:02 +0000

    setup script fixes

--- init_informacam_admin.sh
+++ init_informacam_admin.sh
@@ -1,18 +1,22 @@
-!# /bin/bash
-THIS_DIR=`pwd`
+#! /bin/bash
+ASSET_DIR=`pwd`
+ANNEX_ROOT=$1
+
+# import key and delete
+gpg --allow-secret-key-import --import informacam.gpg.priv_key.file
+rm informacam.gpg.priv_key.file
 
 # add forms to conf
-# import key and delete
+for f in *
+do
+	if echo "$f" | grep '^informacam.form.*' > /dev/null ; then
+		mv $f $ANNEX_ROOT/conf/
+	fi
+done
 
 # create task manually
-curl -X POST http://localhost:9200/unveillance/$TASK_ID -d {\
-	"task_path" : "J3M.forms.initForms", \
-	"queue" : $UV_UUID \
-}
-
-# run task
-curl -X GET http://localhost:8889/task/$TASK_ID
+curl -X PUT http://localhost:8889/task/ -d '{ "task_path" : "J3M.forms.initForms" }'
 
 # run inner script
-cd lib/Annex
-./init_local_remote.sh
+cd $ANNEX_ROOT
+./init_local_remote.sh $ASSET_DIR/unveillance.local_remote.pub_key
