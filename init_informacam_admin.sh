fad565f3c0667f95404de9c938c4450d81446464
harlo <harlo@guardianproject.info>
2014-06-03 11:41:00 +0000

    admin setup fixes

--- init_informacam_admin.sh
+++ init_informacam_admin.sh
@@ -1,10 +1,23 @@
 #! /bin/bash
 ASSET_DIR=`pwd`
 ANNEX_ROOT=$1
+USER_CONFIG=$ANNEX_ROOT/conf/local.config.yaml
+
+if [ $# -eq 2 ]
+then
+	GNUPG_HOME=$2
+else
+	GNUPG_HOME=~/.gnupg
+fi
 
 # import key and delete
+mkdir $GNUPG_HOME
+echo informacam.gpg.homedir: $GNUPG_HOME >> $USER_CONFIG
+echo informacam.gpg.priv_key.password: $(`cat informacam.gpg.priv_key.password`) >> $USER_CONFIG
+
 gpg --allow-secret-key-import --import informacam.gpg.priv_key.file
 rm informacam.gpg.priv_key.file
+rm informacam.gpg.priv_key.password
 
 # add forms to conf
 for f in *
@@ -15,7 +28,7 @@ do
 done
 
 # create task manually
-curl -X PUT http://localhost:8889/task/ -d '{ "task_path" : "J3M.forms.initForms" }'
+curl -X POST http://localhost:8889/task/ -d '{ "task_path" : "J3M.forms.initForms" }'
 
 # run inner script
 cd $ANNEX_ROOT
