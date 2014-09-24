9d106ed8b71e03dcfe2fdf3244a39cd864e8fd57
harlo <harlo@guardianproject.info>
2014-09-24 18:35:04 +0000

    mime type fix (now recognizing application/octet-stream)

--- Tasks/Media/evaluate_text.py
+++ Tasks/Media/evaluate_text.py
@@ -6,7 +6,7 @@ from vars import CELERY_STUB as celery_app
 def evaluateTextFile(task):
 	task_tag = "EVALUATING TEXT FILE"
 	print "\n\n************** %s [START] ******************\n" % task_tag
-	print "image preprocessing at %s" % task.doc_id
+	print "evaluating text file at %s" % task.doc_id
 	task.setStatus(412)
 		
 	from lib.Worker.Models.uv_document import UnveillanceDocument
@@ -41,36 +41,34 @@ def evaluateTextFile(task):
 		un_b64_mime_type = getFileType(un_b64, as_buffer=True)
 		if DEBUG: print "MIME TYPE: %s" % un_b64_mime_type
 		
-		if un_b64_mime_type not in [MIME_TYPES['pgp']]:
+		if un_b64_mime_type not in [MIME_TYPES['pgp'], MIME_TYPES['wildcard']]:
 			print "MIME TYPE NOT USABLE"
 			print "\n\n************** %s [ERROR] ******************\n" % task_tag
 			return
 		
-		if un_b64_mime_type == MIME_TYPES['pgp']:
-			media.addAsset(un_b64, "%s.pgp" % media.file_name,
-				description="un-b64'ed pgp asset")
-				
-			task_path = None
-			task_args = {}
-			message_sentinel = "-----BEGIN PGP MESSAGE-----"
+		media.addAsset(un_b64, "%s.pgp" % media.file_name, description="un-b64'ed pgp asset")
+		
+		task_path = None
+		task_args = {}
+		message_sentinel = "-----BEGIN PGP MESSAGE-----"
+		
+		if un_b64[0:len(message_sentinel)] == message_sentinel:
+			task_path = "PGP.decrypt.decrypt"
+			task_args.update({
+				'pgp_file' : ".data/%s/%s.pgp" % (media._id, media.file_name)
+			})
+		
+		if task_path is not None:
+			from lib.Worker.Models.uv_task import UnveillanceTask
 			
-			if un_b64[0:len(message_sentinel)] == message_sentinel:
-				task_path = "PGP.decrypt.decrypt"
-				task_args.update({
-					'pgp_file' : ".data/%s/%s.pgp" % (media._id, media.file_name)
-				})
+			task_args.update({
+				'task_path' : task_path,
+				'doc_id' : media._id,
+				'queue' : task.queue
+			})
 			
-			if task_path is not None:
-				from lib.Worker.Models.uv_task import UnveillanceTask
-				
-				task_args.update({
-					'task_path' : task_path,
-					'doc_id' : media._id,
-					'queue' : task.queue
-				})
-				
-				new_task = UnveillanceTask(inflate=task_args)
-				new_task.run()
+			new_task = UnveillanceTask(inflate=task_args)
+			new_task.run()
 
 	task.finish()
 	print "\n\n************** %s [END] ******************\n" % task_tag
